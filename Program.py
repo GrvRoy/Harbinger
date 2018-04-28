c3ce322325828b7d5d8e20030ec50a5a800b054d
Gourav Roy <grv.ry21@gmail.com>
2018-04-28 12:50:43 +0530

    Float data fixed

--- Program.py
+++ Program.py
@@ -35,7 +35,7 @@ def input_from_file():
     # x.split()[1] refers to data in second column
     for x in lines:
         max_temp.append(float(x.split('\t')[2]))
-        min_temp.append(x.split('\t')[3].replace("\n", ""))
+        min_temp.append(float(x.split('\t')[3].replace("\n", "")))
     f.close()
     first_10_values_min = min_temp[1:11]
     first_10_values_max = max_temp[1:11]
